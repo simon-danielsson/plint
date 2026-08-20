68c74061d0fa62f13622e486bb955bb4b6066c2e
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-08-20 15:06:37 +0200

    chore(/examples): add routing and include examples

---
+++ examples/5_includes/5_includes.c
@@ -0,0 +1,18 @@
+#define PLINT_IMPLEMENTATION
+#include "../../plint.h"
+
+int main(void) {
+    PlintServer ps = {0};
+
+    Plint_append_route(&ps,
+            &(PlintRoute){.route = "/", .file_path = "index.html"});
+
+    Plint_append_route(&ps, &(PlintRoute){.route = "/favicon.ico",
+            .file_path = "../../media/plint.ico"});
+
+    ServerAddressIPv4 server_addir = {.ip = {127, 0, 0, 1}, .port = 6969};
+
+    PlintServer_start(&ps, server_addir);
+
+    return 0;
+}
